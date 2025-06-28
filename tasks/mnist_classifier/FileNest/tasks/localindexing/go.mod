171169b09b3d07d6d72f81967d9ffef303b8ddc3
Anarghya1610 <anarghya1616@gmail.com>
2025-06-29 00:11:50 +0530

    Delete tasks/mnist_classifier/FileNest directory

--- tasks/mnist_classifier/FileNest/tasks/localindexing/go.mod
+++
@@ -1,5 +0,0 @@
-module localindexing
-
-go 1.24.4
-
-require github.com/lib/pq v1.10.9
--- tasks/mnist_classifier/FileNest/tasks/mnist_classifier/MNIST_Classifier/params_info.py
+++
@@ -1,5 +0,0 @@
-from torchinfo import summary
-from models.small_mobilenet import SmallMobileNet
-
-
-summary(SmallMobileNet(), input_size=(1, 1, 28, 28))
